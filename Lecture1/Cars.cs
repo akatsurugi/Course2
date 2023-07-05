62a9752accf313b380a4c5f59b8a4cf7b70edf00
akatsurugi <babosiuk@gmail.com>
2023-07-05 14:22:02 +0300

    Add project files.

---
+++ Lecture1/Cars.cs
@@ -0,0 +1,13 @@
+namespace Lecture1
+{
+    public class Cars
+    {
+        public DateOnly Date { get; set; }
+
+        public int Cost { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
---
+++ Lecture1/Controllers/CarsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsController : ControllerBase
+    {
+
+        private static readonly string[] Cars = new[]
+        {
+            "Bono", "Kia", "Lancia", "Raven", "Jet", "Blade", "Venom", "Dusk", "Vader", "Black Bob"
+        };
+
+        [HttpGet(Name = "GetCars")]
+        public IEnumerable<Cars> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Cars
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Cost = Random.Shared.Next(5000, 200000),
+                Name = Cars[Random.Shared.Next(Cars.Length)],
+                Description = "Some text for each car",
+            })
+            .ToArray();
+        }
+
+
+
+    }
+}
