fdef874846d50e173ae39a512094442ccad56933
akatsurugi <babosiuk@gmail.com>
2023-08-11 10:11:59 +0300

    Color

---
+++ Colors/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Colors
+{
+    internal class Program
+    {
+        delegate void PrintStringDelegate(string text);
+
+        static void Main(string[] args)
+        {
+            PrintStringDelegate printDelegate = PrintGreenColor;
+            printDelegate("Green Color");
+            printDelegate = PrintOrangeColor;
+            printDelegate("Orange Color");
+        }
+
+        static void PrintGreenColor(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+        }
+
+        static void PrintOrangeColor(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(text);
+        }
+    }
+}
